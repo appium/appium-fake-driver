b192d1acbb80b03b0595b8518b8545f5b16e61e6
Jonathan Lipps <jlipps@gmail.com>
2015-05-27 14:05:29 -0700

    make sure we don't do anything wonky with command exports

--- test/driver-specs.js
+++ test/driver-specs.js
@@ -1,5 +1,6 @@
 // transpile:mocha
 
+import _ from 'lodash';
 import B from 'bluebird';
 import chai from 'chai';
 import chaiAsPromised from 'chai-as-promised';
@@ -7,6 +8,12 @@ import wd from 'wd';
 import 'mochawait';
 import { startServer } from '../..';
 import { TEST_APP, TEST_HOST, TEST_PORT } from './helpers';
+
+import contextCommands from '../lib/commands/contexts';
+import elementCommands from '../lib/commands/element';
+import findCommands from '../lib/commands/find';
+import generalCommands from '../lib/commands/general';
+import exportedCommands from '../lib/commands';
 
 import contextTests from './context-tests';
 import findElementTests from './find-element-tests';
@@ -16,6 +23,25 @@ import alertTests from './alert-tests';
 const should = chai.should();
 const shouldStartServer = process.env.USE_RUNNING_SERVER !== "0";
 chai.use(chaiAsPromised);
+
+describe('Driver commands', () => {
+  let allCommands = [
+    _.keys(contextCommands),
+    _.keys(elementCommands),
+    _.keys(findCommands),
+    _.keys(generalCommands)
+  ];
+  let totalCommands = _.sum(allCommands.map(c => c.length));
+  it('should not overlap between files', () => {
+    _.union(...allCommands).length.should.equal(totalCommands);
+  });
+  it('should export all commands and not leave any out', () => {
+    _.difference(
+      _.union(...allCommands),
+      _.keys(exportedCommands)
+    ).should.eql([]);
+  });
+});
 
 describe('FakeDriver - via HTTP', () => {
   let server = null;
